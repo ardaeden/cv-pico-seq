5f300092a34bab89c25761bdc20cb1898f2c9fba
ardaeden <ardaeden@gmail.com>
2025-12-13 16:05:11 +0300

    Invert encoder rotation direction

--- io.cpp
+++ io.cpp
@@ -10,11 +10,18 @@ constexpr uint64_t DEBOUNCE_US = 20'000;  // 50 ms debounce window
 constexpr uint LED_PIN = 3;               // GP3
 constexpr uint64_t LED_BLINK_DURATION_US = 50'000;  // 50 ms LED on time
 
+constexpr uint ENCODER_CLK = 14;          // GP14
+constexpr uint ENCODER_DATA = 15;         // GP15
+constexpr uint ENCODER_SW = 13;           // GP13
+
 bool button_prev = true;               // starts high because of pull-up
 uint64_t last_button_event_us = 0;     // last time we toggled play state
 
 bool led_blinking = false;             // LED blink state
 uint64_t led_blink_start_us = 0;       // LED blink start timestamp
+
+// Encoder state tracking
+bool encoder_clk_prev = true;          // previous CLK state
 } // namespace
 
 void io_init() {
@@ -53,4 +60,35 @@ void io_update_led() {
             led_blinking = false;
         }
     }
+}
+
+void io_encoder_init() {
+    gpio_init(ENCODER_CLK);
+    gpio_set_dir(ENCODER_CLK, GPIO_IN);
+    gpio_pull_up(ENCODER_CLK);
+
+    gpio_init(ENCODER_DATA);
+    gpio_set_dir(ENCODER_DATA, GPIO_IN);
+    gpio_pull_up(ENCODER_DATA);
+
+    gpio_init(ENCODER_SW);
+    gpio_set_dir(ENCODER_SW, GPIO_IN);
+    gpio_pull_up(ENCODER_SW);
+
+    encoder_clk_prev = gpio_get(ENCODER_CLK);
+}
+
+int io_encoder_poll_delta() {
+    bool clk_now = gpio_get(ENCODER_CLK);
+    
+    // Detect falling edge on CLK
+    if (encoder_clk_prev && !clk_now) {
+        bool data = gpio_get(ENCODER_DATA);
+        encoder_clk_prev = clk_now;
+            // If DATA is low on CLK falling edge -> CCW, if high -> CW
+            return data ? 1 : -1;
+    }
+    
+    encoder_clk_prev = clk_now;
+    return 0;
 }
